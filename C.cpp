f34c438cd4d2cb258debbb7724aa22e34d4be14a
AbhishekYuvi <singhabhishek.7484@gmail.com>
2019-05-31 11:47:28 +0530

    1st

---
+++ C.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int area (int side)
+{
+	return side*side;
+}
+int area (int length , int breadth)
+{
+	return length*breadth;
+}
+int area (int radius)
+{
+	return 3.14*radius*radius;
+}
+int main()
+{
+	cout<<area(5);
+	cout<<area(5,8);
+	cout<<arae(4.9f);
+}
